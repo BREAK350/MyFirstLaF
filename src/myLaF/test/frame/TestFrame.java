e476e3587f270c57bb3d08d388dcf6b218377353
Dima Lymarenko <break350@gmail.com>
2014-12-30 22:22:18 +0200

    Добавив установку UIManager-а.

--- src/myLaF/test/frame/TestFrame.java
+++ src/myLaF/test/frame/TestFrame.java
@@ -3,7 +3,11 @@ package myLaF.test.frame;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+
+import com.gmail.break350.laf.MyLookAndFeel;
 
 public class TestFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -24,7 +28,17 @@ public class TestFrame extends JFrame {
 	}
 
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(MyLookAndFeel.class.getCanonicalName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 		new TestFrame();
 	}
-
 }
