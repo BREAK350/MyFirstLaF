a21c6ba8239f258fc28e44e0a652695690f43dbc
Dima Lymarenko <break350@gmail.com>
2014-12-30 21:54:01 +0200

    Видалив автоматично згенеровані коменти.

--- src/com/gmail/break350/MyLookAndFeel.java
+++ src/com/gmail/break350/MyLookAndFeel.java
@@ -7,31 +7,26 @@ public class MyLookAndFeel extends BasicLookAndFeel {
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getID() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean isNativeLookAndFeel() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isSupportedLookAndFeel() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
