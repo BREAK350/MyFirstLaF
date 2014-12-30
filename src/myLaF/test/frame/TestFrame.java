8ed0ef7bedb3489fb05e11f9338ba340d1799548
Dima Lymarenko <break350@gmail.com>
2014-12-30 22:03:43 +0200

    Добавив пустий конструктор і викликав його в main-і.

--- src/myLaF/test/frame/TestFrame.java
+++ src/myLaF/test/frame/TestFrame.java
@@ -5,8 +5,12 @@ import javax.swing.JFrame;
 public class TestFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 
-	public static void main(String[] args) {
+	public TestFrame() {
 
+	}
+
+	public static void main(String[] args) {
+		new TestFrame();
 	}
 
 }
