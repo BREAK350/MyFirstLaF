999a8cd091ee23327b119b1da161de6090fb78c8
Dima Lymarenko <break350@gmail.com>
2014-12-30 22:18:06 +0200

    Добавив панель на фрейм і кнопку на неї.

--- src/myLaF/test/frame/TestFrame.java
+++ src/myLaF/test/frame/TestFrame.java
@@ -1,6 +1,9 @@
 package myLaF.test.frame;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class TestFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -8,11 +11,16 @@ public class TestFrame extends JFrame {
 	public TestFrame() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(800, 600);
+		initComponents();
 		setVisible(true);
 	}
 
 	public void initComponents() {
-
+		JPanel content = new JPanel();
+		content.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(content);
+		JButton btn = new JButton("Button");
+		content.add(btn);
 	}
 
 	public static void main(String[] args) {
