694f01394ee71cb31d6f01bdf32b3fb43ba11a38
Dima Lymarenko <break350@gmail.com>
2014-12-30 22:06:43 +0200

    Зробив фрейм видимим розміром 800 на 600. Добавив дефоултну дію при закриті - завершення джава-програми.

--- src/myLaF/test/frame/TestFrame.java
+++ src/myLaF/test/frame/TestFrame.java
@@ -6,7 +6,9 @@ public class TestFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public TestFrame() {
-
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
