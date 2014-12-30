d3ee9f1ddb87bc27ab787277dde2bf34a01a95a1
Dima Lymarenko <break350@gmail.com>
2014-12-30 22:40:59 +0200

    Добавив "увесь" вміст методу initClassDefaults, тобто, установка своїх лафів для усіх візуальних компонентів.

--- src/com/gmail/break350/laf/MyLookAndFeel.java
+++ src/com/gmail/break350/laf/MyLookAndFeel.java
@@ -34,6 +34,73 @@ public class MyLookAndFeel extends BasicLookAndFeel {
 	@Override
 	protected void initClassDefaults(UIDefaults table) {
 		super.initClassDefaults(table);
+		// // Label
+		// table.put ( "LabelUI", ... );
+		// table.put ( "ToolTipUI", ... );
+		//
+		// // Button
 		table.put("ButtonUI", MyButtonUI.class.getCanonicalName());
+		// table.put ( "ToggleButtonUI", ... );
+		// table.put ( "CheckBoxUI", ... );
+		// table.put ( "RadioButtonUI", ... );
+		//
+		// // Menu
+		// table.put ( "MenuBarUI", ... );
+		// table.put ( "MenuUI", ... );
+		// table.put ( "PopupMenuUI", ... );
+		// table.put ( "MenuItemUI", ... );
+		// table.put ( "CheckBoxMenuItemUI", ... );
+		// table.put ( "RadioButtonMenuItemUI", ... );
+		// table.put ( "PopupMenuSeparatorUI", ... );
+		//
+		// // Separator
+		// table.put ( "SeparatorUI", ... );
+		//
+		// // Scroll
+		// table.put ( "ScrollBarUI", ... );
+		// table.put ( "ScrollPaneUI", ... );
+		//
+		// // Text
+		// table.put ( "TextFieldUI", ... );
+		// table.put ( "PasswordFieldUI", ... );
+		// table.put ( "FormattedTextFieldUI", ... );
+		// table.put ( "TextAreaUI", ... );
+		// table.put ( "EditorPaneUI", ... );
+		// table.put ( "TextPaneUI", ... );
+		//
+		// // Toolbar
+		// table.put ( "ToolBarUI", ... );
+		// table.put ( "ToolBarSeparatorUI", ... );
+		//
+		// // Table
+		// table.put ( "TableUI", ... );
+		// table.put ( "TableHeaderUI", ... );
+		//
+		// // Chooser
+		// table.put ( "ColorChooserUI", ... );
+		// table.put ( "FileChooserUI", ... );
+		//
+		// // Container
+		// table.put ( "PanelUI", ... );
+		// table.put ( "ViewportUI", ... );
+		// table.put ( "RootPaneUI", ... );
+		// table.put ( "TabbedPaneUI", ... );
+		// table.put ( "SplitPaneUI", ... );
+		//
+		// // Complex components
+		// table.put ( "ProgressBarUI", ... );
+		// table.put ( "SliderUI", ... );
+		// table.put ( "SpinnerUI", ... );
+		// table.put ( "TreeUI", ... );
+		// table.put ( "ListUI", ... );
+		// table.put ( "ComboBoxUI", ... );
+		//
+		// // Desktop pane
+		// table.put ( "DesktopPaneUI", ... );
+		// table.put ( "DesktopIconUI", ... );
+		// table.put ( "InternalFrameUI", ... );
+		//
+		// // Option pane
+		// table.put ( "OptionPaneUI", ... );
 	}
 }
