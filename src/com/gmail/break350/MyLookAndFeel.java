ed9bb1215964f5d6f7f42b7914b5ace036e13c74
Dima Lymarenko <break350@gmail.com>
2014-12-30 21:53:16 +0200

    Перший коміт. Створення MyLookAndFeel.

---
+++ src/com/gmail/break350/MyLookAndFeel.java
@@ -0,0 +1,38 @@
+package com.gmail.break350;
+
+import javax.swing.plaf.basic.BasicLookAndFeel;
+
+public class MyLookAndFeel extends BasicLookAndFeel {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isNativeLookAndFeel() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSupportedLookAndFeel() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
