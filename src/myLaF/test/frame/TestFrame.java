79ab830959cacf3cf89219d0f119b25c554fb56b
Dima Lymarenko <break350@gmail.com>
2014-12-30 22:02:39 +0200

    Добавив пустий фрейм для тесту laf-а.

---
+++ src/myLaF/test/frame/TestFrame.java
@@ -0,0 +1,12 @@
+package myLaF.test.frame;
+
+import javax.swing.JFrame;
+
+public class TestFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+
+	}
+
+}
