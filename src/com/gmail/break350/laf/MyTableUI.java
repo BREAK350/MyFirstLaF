a705aeab12a529f9597d52942752904be974f0c3
Dima Lymarenko <break350@gmail.com>
2014-12-31 15:06:42 +0200

    Встановив рендер для стовпця в методі paint класа MyTableUI.

--- src/com/gmail/break350/laf/MyTableUI.java
+++ src/com/gmail/break350/laf/MyTableUI.java
@@ -2,12 +2,15 @@ package com.gmail.break350.laf;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JTable;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicTableUI;
+import javax.swing.table.TableColumnModel;
 
 public class MyTableUI extends BasicTableUI {
 	@Override
@@ -22,7 +25,15 @@ public class MyTableUI extends BasicTableUI {
 
 	@Override
 	public void paint(Graphics g, JComponent c) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 
+		JTable table = (JTable) c;
+		TableColumnModel tcm = table.getColumnModel();
+		tcm.getColumn(0).setCellRenderer(new MyTableCellRender());
+		g2d.setPaint(Color.RED);
+		g2d.drawRect(2, 2, 30, 30);
 		super.paint(g, c);
 	}
 
