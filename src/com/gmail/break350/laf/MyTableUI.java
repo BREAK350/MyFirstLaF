cf1416dae59500ac4484f875ce6b5cf0dcbaee17
Dima Lymarenko <break350@gmail.com>
2014-12-31 12:28:36 +0200

    Добавив статичний метод createUI() в класі MyTableUI.

--- src/com/gmail/break350/laf/MyTableUI.java
+++ src/com/gmail/break350/laf/MyTableUI.java
@@ -1,11 +1,16 @@
 package com.gmail.break350.laf;
 
 import javax.swing.JComponent;
+import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicTableUI;
 
 public class MyTableUI extends BasicTableUI {
 	@Override
 	public void installUI(JComponent c) {
 		super.installUI(c);
+	}
+
+	public static ComponentUI createUI(JComponent c) {
+		return new MyTableUI();
 	}
 }
