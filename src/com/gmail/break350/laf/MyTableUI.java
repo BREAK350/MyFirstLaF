7c5688a45c1ea34ac956d3e774c19abffb552b8e
Dima Lymarenko <break350@gmail.com>
2014-12-31 12:42:48 +0200

    Встановив для MyTableUI: opaque = false; focusable = true; border = emptyBorder(4,4,4,4); gridColor = 0xe6e6e6.

--- src/com/gmail/break350/laf/MyTableUI.java
+++ src/com/gmail/break350/laf/MyTableUI.java
@@ -1,6 +1,10 @@
 package com.gmail.break350.laf;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
+import javax.swing.JTable;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicTableUI;
 
@@ -8,6 +12,11 @@ public class MyTableUI extends BasicTableUI {
 	@Override
 	public void installUI(JComponent c) {
 		super.installUI(c);
+		JTable table = (JTable) c;
+		table.setOpaque(false);
+		table.setFocusable(true);
+		table.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+		table.setGridColor(new Color(0xe6e6e6));
 	}
 
 	public static ComponentUI createUI(JComponent c) {
