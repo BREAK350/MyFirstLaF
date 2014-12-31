13ba5f7699f2cb39abb99ce5789c4358b5b6e292
Dima Lymarenko <break350@gmail.com>
2014-12-31 12:25:00 +0200

    Добавив метод installUI в класі MyTableUI, що викликає метод super.installUI.

--- src/com/gmail/break350/laf/MyTableUI.java
+++ src/com/gmail/break350/laf/MyTableUI.java
@@ -1,7 +1,11 @@
 package com.gmail.break350.laf;
 
+import javax.swing.JComponent;
 import javax.swing.plaf.basic.BasicTableUI;
 
 public class MyTableUI extends BasicTableUI {
-
+	@Override
+	public void installUI(JComponent c) {
+		super.installUI(c);
+	}
 }
